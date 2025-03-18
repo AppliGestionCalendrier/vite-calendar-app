4d026f72fbde795bc5c9163d367757794823fa09
Alexandre Bernardini <bernardini.alexandre2003@gmail.com>
2025-03-18 14:33:15 +0100

    changement de l'url

--- src/components/CalendarList.tsx
+++ src/components/CalendarList.tsx
@@ -26,7 +26,7 @@ const CalendarList: React.FC = () => {
     const handleAddICal = async (): Promise<void> => {
         if (icalUrl.trim() !== '') {
             try {
-                const response = await fetch(`https://ton-projet.vercel.app/api/events?url=${encodeURIComponent(icalUrl)}`);
+                const response = await fetch(`https://vite-calendar-app-seven.vercel.app//api/events?url=${encodeURIComponent(icalUrl)}`);
                 if (!response.ok) {
                     throw new Error('Erreur lors de la récupération du nom du calendrier');
                 }
